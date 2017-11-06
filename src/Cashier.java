120d6bdea35208a523c1eec931326e55cb72d597
dirtybits <dirtybitsofficial@gmail.com>
2017-11-06 10:21:43 -0600

    Initial commit

---
+++ src/Cashier.java
@@ -0,0 +1,19 @@
+public class Cashier {
+
+    private boolean isBusy;
+    private int customerServed;
+
+    Cashier(){
+
+    }
+
+    public int getServeTime(){
+
+        return serveTime;
+    }
+
+    public void isReady(){
+
+    }
+
+}
