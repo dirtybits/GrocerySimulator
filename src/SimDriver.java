487ed1c65f5e7a26fcad6e712dcba2a0f4534410
BMD-PC <bendoyle21@hotmail.com>
2017-11-18 15:03:40 -0600

    /

--- src/SimDriver.java
+++ src/SimDriver.java
@@ -1,12 +1,31 @@
-public class SimDriver {
+import java.util.Random;
 
+public class SimDriver {
+    public boolean keepRunning = true;
     public static void main(String args[]){
 
+        SimDriver sd = new SimDriver();
         CashierManager manager = new CashierManager();
 
+        while (sd.keepRunning){
+            Double value = Math.random();
+            if (value > 0.75){
+                // add a new customer
+                Customer c = new Customer();
+                manager.addCustomer(c);
 
+            }else{
+                try {
+                    Thread.sleep(1000
 
+                    );
+                } catch (InterruptedException e) {
+                    //.sleep can throw and exception
+                }
 
+            }
+            manager.rearangeLines();
+        }
     }
 
 }
