9ac38e9fcfb23c8a6d70b0a1fd6c18db351b32a8
BMD-PC <bendoyle21@hotmail.com>
2017-11-18 12:44:08 -0600

    Ben Test

--- src/Cashier.java
+++ src/Cashier.java
@@ -1,16 +1,38 @@
-public class Cashier {
+public class Cashier extends Thread{
 
-    private boolean isBusy;
-    private int customerServed;
+    private boolean _isBusy;
+    private int _customerServed;
+    private boolean _stop;
 
     Cashier(){
-        // this is a comment
-        // this is a second comment
+        _isBuist = false;
+        _customerServed = 0;
+        _stop = false;
     }
-
 
     public void isReady(){
 
     }
 
+    public void run(){
+        System.out.println(Thread.currentThread().getName() + " Started");
+        runner();
+    }
+    public void kill() {
+        stop = true;
+    }
+
+    private void runner() {
+        while (stop != true) {
+            // do cashier stuff here
+
+            try {
+                this.currentThread().sleep(100
+
+                );
+            } catch (InterruptedException e) {
+                //.sleep can throw and exception
+            }
+        }
+    }
 }
