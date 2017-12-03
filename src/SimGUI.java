9ed3cf3f7b2c990deb2223c76418930b97d2e1a6
dirtybits <dirtybitsofficial@gmail.com>
2017-12-03 13:11:08 -0600

    Fixed type rearangeLines() to rearrangeLines()

--- src/SimGUI.java
+++ src/SimGUI.java
@@ -1,6 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 
 public class SimGUI {
+
     private JPanel panel1;
     private JProgressBar progressBar1;
+    private JProgressBar progressBar2;
+    private JProgressBar progressBar3;
+    private JProgressBar progressBar4;
+    private JProgressBar progressBar5;
+    private JButton startSimulatorButton;
+    private JTextArea textArea1;
+
+
+    public SimGUI() {
+        startSimulatorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               try{
+                   SimDriver.main(new String[0]);
+               } catch (Exception ex){
+                   ex.printStackTrace();
+               }
+            }
+        });
+    }
 }
